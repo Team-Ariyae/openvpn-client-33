c059b5a203662de4e5e5a0a3be119ad199ada90f
MehraB832 <mehrabshafae@gmail.com>
2024-05-13 17:40:47 +0330

    Revert "add new openvpn3"
    This reverts commit 2b219d6f

--- openvpn/src/main/java/de/blinkt/openvpn/core/NativeUtils.java
+++ openvpn/src/main/java/de/blinkt/openvpn/core/NativeUtils.java
@@ -39,7 +39,7 @@ public class NativeUtils {
 
     static {
         if (!isRoboUnitTest()) {
-            System.loadLibrary("ovpnutil");
+            System.loadLibrary("opvpnutil");
             if (Build.VERSION.SDK_INT == Build.VERSION_CODES.JELLY_BEAN)
                 System.loadLibrary("jbcrypto");
 
